81f6ffc665f30cd51277d2ad5ebf5319f4321c01
Jahaxundov <jahangirea@code.edu.az>
2023-07-18 03:29:32 +0400

    Added homework

---
+++ HomeWork 17.07.23/HomeWork/Program.cs
@@ -0,0 +1,37 @@
+﻿//String qebul eden bir method yazirsiz. Hemin method qebul etdiyi stringin ilk herfini boyuk yazdirmalidir. (salam gelirse ekranda Salam gostermelidir)
+
+//Console.WriteLine("Adi daxil edin");
+//string text = Console.ReadLine();
+//Console.WriteLine("------------");
+//string result = ToUpperFirstChar(text);
+//Console.WriteLine(result);
+
+
+//static string ToUpperFirstChar(string text)
+//{
+//    text = Char.ToUpper(text[0]) + text.Substring(1);
+//    return text;
+//}
+
+
+//Parametr olaraq string qebul eden metod yazmalisiz. Hemin metod qebul etdiyi stringi tersine formada ekranda gostersin. (Cavid gelirse , ekranda divaC gorsensin).
+
+//Console.WriteLine("Sozu daxil et :");
+//string text = Console.ReadLine();
+//Console.WriteLine("------");
+//string result = ReverseWord(text);
+//Console.WriteLine(result);
+
+//static string ReverseWord(string text)
+//{
+//    char[] arr = text.ToCharArray();
+//    Array.Reverse(arr);
+//    string str = new string(arr);
+//    return str;
+
+//}
+
+
+
+
+
